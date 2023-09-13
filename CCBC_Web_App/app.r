a1fcac989f3ba329424c3673aa49f96687130289
Chris <113730877+chrisewanik@users.noreply.github.com>
2023-09-13 14:31:37 -0600

    Added Shiny Dashboard and First Skeleton

--- CCBC_Web_App/app.r
+++ CCBC_Web_App/app.r
@@ -1,17 +1,50 @@
-# Load necessary libraries
+# Load required libraries
 library(shiny)
+library(shinydashboard)
 
 # Define UI for the app
-ui <- fluidPage(
-    includeCSS("www/style.css"), # Include custom CSS
-    titlePanel("Canadian Baseball Connection"),
-    # Other UI elements here
+ui <- dashboardPage(
+    
+    # Navbar
+    dashboardHeader(title = "Canadian Baseball Connection"),
+    
+    # Sidebar
+    dashboardSidebar(
+        sidebarMenu(id = "someID",
+                    selectInput("year", "Select Year", choices = c("2021", "2020")),
+                    selectInput("season", "Select Season", choices = c("Spring", "Summer"))
+                    # ... additional code
+        )
+    ),
+    
+    # Main Panel
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "dashboard",
+                    fluidRow(
+                        box(
+                            title = "Table",
+                            dataTableOutput("stats_table")
+                        ),
+                        box(
+                            title = "Plot 1",
+                            plotOutput("plot1")
+                        ),
+                        box(
+                            title = "Plot 2",
+                            plotOutput("plot2")
+                        )
+                    )
+            )
+            # Add more tabItems as needed
+        )
+    )
 )
 
 # Define server logic for the app
-server <- function(input, output) {
-    # Server code here
+server <- function(input, output, session) {
+    # Placeholder for server logic
 }
 
 # Run the app
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
