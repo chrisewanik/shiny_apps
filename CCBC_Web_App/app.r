703542fe21633a50d9edec17389c9ef2e1fc3ead
Chris <113730877+chrisewanik@users.noreply.github.com>
2023-09-14 08:20:06 -0600

    Fixed CSS, added themes

--- CCBC_Web_App/app.r
+++ CCBC_Web_App/app.r
@@ -1,53 +1,64 @@
 # Load required libraries
 library(shiny)
 library(shinydashboard)
+library(shinythemes)
 
 # Define UI for the app
-ui <- 
-    dashboardPage(
-    
-    # Navbar
-    dashboardHeader(title = "Canadian Baseball Connection"),
-    
-    # Sidebar
-    dashboardSidebar(
-        tags$head(
-            tags$link(rel = "stylesheet", type = "text/css", href = "www/style.css")
-        ),
-        sidebarMenu(id = "someID",
-                    selectInput("year", "Select Year", choices = c("2021", "2020")),
-                    selectInput("season", "Select Season", choices = c("Spring", "Summer")),
-                    selectInput("team", "Select Team", choices = c("PBA", "OC", "VIU"))
-                    # ... additional code
-        )
+ui <- fluidPage(
+    # Title
+    theme = shinytheme("slate"),
+    # themeSelector(),
+    # Custom CSS
+    tags$head(
+        tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
     ),
     
-    # Main Panel
-    dashboardBody(
-        tags$head(
-            tags$link(rel = "stylesheet", type = "text/css", href = "www/style.css")
-        ),
-        tabItems(
-            tabItem(tabName = "dashboard",
-                    fluidRow(
-                        box(
-                            title = "Table",
-                            width = 12,
-                            dataTableOutput("stats_table")
-                        ),
+    navbarPage(title = "PBA Analytics",
+               tabPanel("Player Stats",
+            dashboardPage(
+            
+            # Navbar
+            dashboardHeader(title = "Player Stats"),
+            
+            
+            # Sidebar
+            dashboardSidebar(
+                tags$head(
+                    tags$link(rel = "stylesheet", type = "text/css", href = "www/style.css")
+                ),
+                sidebarMenu(id = "sidebar_menu",
+                            selectInput("year", "Select Year", choices = c("2021", "2020")),
+                            selectInput("season", "Select Season", choices = c("Spring", "Summer")),
+                            selectInput("team", "Select Team", choices = c("PBA", "OC", "VIU")),
+                            selectInput("category", "Stat Category", choices = c("Hitting", "Pitching"))
+                )
+            ),
+            
+            # Main Panel
+            dashboardBody(
+                tags$head(
+                    tags$link(rel = "stylesheet", type = "text/css", href = "www/style.css")
+                ),
+                fluidRow(
+                    box(
+                        title = "Table",
+                        width = 12,
+                        height = 400,
+                        dataTableOutput("stats_table")
                     ),
-                    fluidRow(
-                        box(
-                            title = "Plot 1",
-                            plotOutput("plot1")
-                        ),
-                        box(
-                            title = "Plot 2",
-                            plotOutput("plot2")
+                ),
+                fluidRow(
+                    box(
+                        title = "Plot 1",
+                        plotOutput("plot1")
+                    ),
+                    box(
+                        title = "Plot 2",
+                        plotOutput("plot2")
                         )
                     ),
+                )
             )
-            # Add more tabItems as needed
         )
     )
 )
